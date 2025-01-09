ece8f4563a18b751a1f9021d0fae9c1f331e1d39
Bhabani Shankar Barik <bhabanishankar.barik21@gmail.com>
2025-01-09 16:40:39 +0530

    Flour Packer

---
+++ Control Flow/Exercises/FlourPacker.java
@@ -0,0 +1,30 @@
+/*write a small program, that given a number of small and large flour bags and a target weight, it can determine if a package can be assembled.*/
+
+package Exercises;
+
+public class FlourPacker {
+
+    private static final int BIG_WEIGHT = 5;
+
+    public static boolean canPack(int bigCount, int smallCount, int goal) {
+
+        if (bigCount < 0 || smallCount < 0 || goal < 0) {
+            return false;
+        }
+
+        boolean result = false;
+        int totalBigWeight = bigCount * BIG_WEIGHT;
+        if (totalBigWeight >= goal) {
+            int remaining = goal % BIG_WEIGHT;
+            if (smallCount >= remaining) {
+                result = true;
+            }
+        } else {
+            if (smallCount >= goal - totalBigWeight) {
+                result = true;
+            }
+        }
+        return result;
+    }
+}
+
