ad35498628d33462b19ddde12b0a40dbe2d3c0f2
Bhabani Shankar Barik <bhabanishankar.barik21@gmail.com>
2025-01-09 16:40:59 +0530

    Greatest Common Divisor

---
+++ Control Flow/Exercises/GreatestCommonDivisor.java
@@ -0,0 +1,32 @@
+/*
+write a small program that returns the greatest common divisor of two numbers.
+
+For example 12 and 30:
+
+12 can be divided by 1, 2, 3, 4, 6, 12
+
+30 can be divided by 1, 2, 3, 5, 6, 10, 15, 30
+
+The greatest common divisor is 6 since both 12 and 30 can be divided by 6, and there is no resulting remainder.
+*/
+
+package Exercises;
+
+public class GreatestCommonDivisor {
+
+    public static int getGreatestCommonDivisor(int first, int second) {
+
+        if (first < 10 || second < 10) {
+            return -1;
+        }
+
+        int min = first < second ? first : second;
+        int gcd = 1;
+        for (int j = 1; j <= min; j++) {
+            if (first % j == 0 && second % j == 0) {
+                gcd = j;
+            }
+        }
+        return gcd;
+    }
+}
