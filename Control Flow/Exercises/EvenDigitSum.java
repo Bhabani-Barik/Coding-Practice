2ebd1fee0b281e924dae03bf742e1db43897b982
Bhabani Shankar Barik <bhabanishankar.barik21@gmail.com>
2025-01-09 16:39:52 +0530

    Even Digit Sum

---
+++ Control Flow/Exercises/EvenDigitSum.java
@@ -0,0 +1,37 @@
+/*
+write a small program that calculates the sum of the even digits of a given number.
+
+
+
+*/
+
+package Exercises;
+
+public class EvenDigitSum {
+    public static int getEvenDigitSum(int number) {
+
+        if (number < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+        for (int i = number; i > 0; i /= 10) {
+            int lastDigit = i % 10;
+            if (lastDigit % 2 == 0) {
+                sum += lastDigit;
+            }
+        }
+
+        // == alternative using while loop ==
+        // while (number > 0) {
+        //     int lastDigit = number % 10;
+        //     if (lastDigit % 2 == 0) {
+        //         sum += lastDigit;
+        //     }
+        //     number /= 10;
+        // }
+
+        return sum;
+    }
+}
+
